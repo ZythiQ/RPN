d9dad46cca406ad38f8973ee15468908158d8e55
Zach Van Horn <zvanhorn10@gmail.com>
2024-11-24 23:43:30 -0500

    Swapping to stack implementation for non-recursive execution (less jank).

--- src/interfaces/context.py
+++ src/interfaces/context.py
@@ -1,12 +1,11 @@
-from collections import deque
 from interfaces.expression import Expression
+from collections import deque
 
 class Context():
     '''Maintain and execute the program.'''
 
     # ===== Attributes ===== #
-    program:list[Expression]
-    memory:deque
+    stack:deque
     binds:dict
     hand:deque
     log:bool
@@ -14,29 +13,29 @@ class Context():
     # ===== Methods ===== #
     def __init__(self, program:list[Expression], log:bool=False):
         '''Context constructor.'''
-        self.program = program
-        self.memory = deque()
+        self.stack = deque()
         self.binds = dict()
         self.hand = deque()
         self.log = log
 
-    def execute(self):
-        '''Execute the program.'''
+        self.stack.extendleft(program)
+
+    def step(self):
+        '''Execute the next step.'''
+        pass
+
+    def push(self, exp:Expression):
+        '''Push the expression onto the stack.'''
         pass
 
     def touch(self, exp:Expression):
         '''Push the expression into program hand.'''
         pass
 
-    def queue(self, exp:Expression):
-        '''Queue the expression into program memory.'''
-        pass
-
     def bind(self, exp:Expression, key:str=None):
         '''Bind the expression to the key. Unbind if no key.'''
         pass
 
-    # ===== Dunders ===== #
     def __str__(self) -> str:
         '''Represent the current context as a string.'''
         pass
